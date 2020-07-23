ff3f3c420f547681a8da8f7703bc7b4d57aa7998
Sarah McLaughlin <smmclau3@ncsu.edu>
2020-07-23 14:28:21 -0500

    Added UI and Server data for "Data Exploration" and "Data" Tabs

--- McLaughlinApp/server.R
+++ McLaughlinApp/server.R
@@ -6,18 +6,73 @@ library(shiny)
 library(DT)
 library(tidyverse)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# Bring in Data  
+data <- read_delim("student-mat.csv", delim = ";")
 
-    output$distPlot <- renderPlot({
+data <- mutate(data, letter = 
+                   ifelse(G3 >= 16, "A", 
+                          ifelse(G3 >= 14, "B", 
+                                 ifelse(G3 >= 12, "C", 
+                                        ifelse(G3 >= 10, "D", 
+                                               "F")))))
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+# Create Pass Fail Variable  
+data <- mutate(data, final = ifelse(G3>= 10, "Pass", "Fail"))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Make certain variables factors
+data$letter <- as.factor(data$letter)
+data$final <- as.factor(data$final)
+data$sex <- as.factor(data$sex)
+data$school <- as.factor(data$school)
+data$Pstatus <- as.factor(data$Pstatus)
+data$famsize <- as.factor(data$famsize)
+data$internet <- as.factor(data$internet)
+data$higher <- as.factor(data$higher)
 
+
+# Select Only Specific Variables 
+data <- data %>% select(final, letter, G3, sex, age, school, absences, Pstatus, Medu, Fedu, famsize, famrel, studytime, failures, internet, higher, traveltime, Walc, health)
+
+# ------------------------------------------ # 
+
+# Define server logic for each tab 
+shinyServer(function(input, output, session) {
+
+    getData <- reactive({
+        data2 <- data %>% select(input$CatTable)
+        })
+    
+    # Output for Tab 2 
+    output$catTable <- renderTable({
+        
+    # Get filtered data 
+        newData <- getData()
+        
+    # One Way Table
+    table(select(newData, input$CatTable))
     })
-
-})
+    
+    output$catGraph <- renderPlot({
+        #Not colored by a variable 
+        if (as.character(input$CatCol) == "None"){
+            # Bar graphs of pass fail 
+            g <- ggplot(data = data, aes_string(x = input$CatTable))
+            # Not by school  
+            g + geom_bar()
+        } else {
+            g <- ggplot(data = data, aes_string(x = input$CatTable))
+            # By School 
+            g + geom_bar(aes_string(fill = input$CatCol))
+        }
+    })
+    
+    # Output for Tab 5 
+    output$tab5 <- renderTable({
+        data
+    })
+    ### Fix this later
+  # observe( if (input$save){
+       # ggsave(filename = "Dataset")}  
+       # else {}
+   
+    })
--- McLaughlinApp/ui.R
+++ McLaughlinApp/ui.R
@@ -4,26 +4,95 @@
 #
 
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
+# UI 
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+   titlePanel("McLauglin APP: Using Data Mining to Predict Secondary School Performance in Math Class"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+    # Create Dashboard  
+    dashboardPage(
+        dashboardHeader(title = "Dashboard"),
+        dashboardSidebar(
+            sidebarMenu(
+                # First Tab 
+                menuItem("Introduction", tabName = "Intro"),
+                # Second Tab 
+                menuItem("Data Exploration", tabName = "DE", 
+                         menuSubItem("Categorial Data", tabName = "Cat"), 
+                         menuSubItem("Quantitative Data", tabName = "Quant")), 
+                # Third Tab 
+                menuItem("Principal Component Analysis", tabName = "PCA"), 
+                # Fourth Tab 
+                menuItem("Modeling", tabName = "Model"), 
+                # Fifth Tab 
+                menuItem("Data", tabName = "D")
+            )
+        ), 
+        
+        # Dashboard Body 
+        dashboardBody(
+            tabItems(
+                
+                # Categorical Data Analysis Tab 
+                tabItem(tabName = "Cat", 
+                        fluidRow(
+                            # Type of Data Widget
+                            box(title = "Analysis of Categorical Data", 
+                                # Select Widget Input 
+                                selectInput("CatTable", 
+                                           "Pick Variable", 
+                                            choices = list("final", "letter", "sex","age", "school", "Pstatus", "internet", "higher"), 
+                                            multiple = FALSE)), 
+                            # One Way Table 
+                            box(title = "One Way Table", 
+                                tableOutput("catTable")), 
+                            
+                            # Bar Graphs 
+                            box(title = "Bar Graphs", 
+                                # Click to Sort by a Variable 
+                                selectInput("CatCol", 
+                                            "Color by Variable", 
+                                            choices = list("None", "sex", "school", "Pstatus", "internet", "higher"), 
+                                            multiple = FALSE), 
+                                plotOutput("catGraph"))
+                        )),
+                
+                # Quantitative Data Analysis Tab 
+                tabItem(tabname = "Quant", 
+                        fluidRow( 
+                            box(
+                                #
+                            ))), 
+                
+                # Fifth Tab 
+                tabItem(tabName = "D",
+                        # Widget to Save Data Set
+                        box( 
+                            actionButton("save", "Save Dataset")), 
+                        
+                        # Show DataSet
+                        box(title = "Data", 
+                            tableOutput("tab5")))
+            )
         )
     )
-))
+    
+    # Sidebar with ability for user to pick categorial or quantitative
+
+        # Show a plot of the generated distribution
+       # mainPanel(
+           # tabsetPanel(
+              #  tabPanel("Information", ), 
+               # tabPanel("Data Exploration"), 
+               # tabPanel("Principal Component Analysis"), 
+               # tabPanel("Modeling"), 
+               # tabPanel("Data"))
+           # )
+      #  )
+   # )
+  )
+)
+
