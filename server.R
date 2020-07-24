f566698f314e80e249a57da279709eec50662d83
Sarah McLaughlin <smmclau3@ncsu.edu>
2020-07-24 15:27:09 -0500

    Moved out of app folder and into repo

---
+++ server.R
@@ -0,0 +1,196 @@
+# Sarah McLaughlin 
+# ST558 Project 3
+# July 23, 2020 
+
+library(shiny)
+library(DT)
+library(tidyverse)
+library(googleVis)
+library(devtools)
+
+# Bring in Data  
+data <- read_delim("student-mat.csv", delim = ";")
+
+data <- mutate(data, letter = 
+                   ifelse(G3 >= 16, "A", 
+                          ifelse(G3 >= 14, "B", 
+                                 ifelse(G3 >= 12, "C", 
+                                        ifelse(G3 >= 10, "D", 
+                                               "F")))))
+
+# Create Pass Fail Variable  
+data <- mutate(data, final = ifelse(G3>= 10, "Pass", "Fail"))
+
+# Make certain variables factors
+data$letter <- as.factor(data$letter)
+data$final <- as.factor(data$final)
+data$sex <- as.factor(data$sex)
+data$school <- as.factor(data$school)
+data$Pstatus <- as.factor(data$Pstatus)
+data$famsize <- as.factor(data$famsize)
+data$internet <- as.factor(data$internet)
+data$higher <- as.factor(data$higher)
+
+
+# Select Only Specific Variables 
+data <- data %>% select(final, letter, G3, sex, age, school, absences, Pstatus, Medu, Fedu, famsize, famrel, studytime, failures, internet, higher, traveltime, Walc, health)
+
+# ------------------------------------------ # 
+
+# Define server logic for each tab 
+shinyServer(function(input, output, session) {
+
+
+# ---------- TAB 1 ---------------- #
+    
+    
+# ---------- TAB 2 ---------------- # 
+    getData <- reactive({
+        data2 <- data %>% select(input$CatTable)
+        })
+    
+    # Output for Tab 2 
+    output$catTable <- renderTable({
+        
+    # Get filtered data 
+        newData <- getData()
+        
+    # One Way Table
+    table(select(newData, input$CatTable))
+    })
+    
+    # Bar Graphs
+    output$catGraph <- renderPlot({
+        
+        # Colored by variable 
+        if (input$color){
+            g <- ggplot(data = data, aes_string(x = input$CatTable))
+            #By color
+            g + geom_bar(aes_string(fill = input$CatCol))
+            
+        } else {
+        # Bar graphs of variable 
+        g <- ggplot(data = data, aes_string(x = input$CatTable))
+        # Not by color 
+        g + geom_bar()
+        }
+    })
+    
+    # Output for Numerical analysis  
+    output$numsums <- renderGvis({
+        numData <- data %>% select(G3, age, absences, Medu, Fedu, famrel, studytime, 
+                                   failures, traveltime, Walc, health)
+        mat <- apply(numData, 2, summary, digits = 2, na.rm = TRUE)
+        mat <- cbind(' ' = c("Min", "1st Q", "Median", "Mean", "3rd Q", "Max"), mat)
+        mat <- as.data.frame(mat) 
+        
+        # Just one variable 
+        if (input$one){
+            oneMat <- cbind(' ' = c("Min", "1st Q", "Median", "Mean", "3rd Q", "Max"), mat[input$oneNum])
+            gvisTable(oneMat)
+        } else {
+        # All variables 
+            gvisTable(mat)
+        }
+    })
+    
+    # Histogram
+    output$Hist <- renderPlot({
+        if(input$one) {
+        g <- ggplot(data = data, aes_string(x = input$oneNum))
+        
+        g + geom_histogram(stat = "count")
+        }else {}
+        })
+    
+    # Scatter Plot 
+    output$scatter <- renderPlot({
+        # Create Scatter Plot 
+        g <- ggplot(data = data, 
+                        aes_string(x = input$xvar, y = input$yvar))
+        g + geom_point(position = "jitter")}
+    )
+    
+    # Info for click 
+    output$info <- renderText({
+        paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    })
+    
+    # Create Reactive function to use to save plot  
+    plotInput <- reactive({
+        g <- ggplot(data = data, 
+                    aes_string(x = input$xvar, y=input$yvar))
+        g + geom_point(position = "jitter")
+    })
+    
+    # Output for Saving Scatterplot 
+       output$savescat <- downloadHandler(
+          filename <- function(){
+              paste(input$xvar, "by", input$yvar, ".png", sep = "")
+         }, 
+          content <-function(file){
+              ggsave(file, plot = plotInput())
+          }
+       )
+    
+# -------------- TAB 3 ------------ #
+       output$PCAOutput <- renderPrint({
+           if (length(input$PCAVar) == 0){
+               return("Pick a variable to begin Principal Component Analysis")
+           } else {
+        # PCA Function  
+        PCs <- prcomp(select(data, !!!input$PCAVar), scale = TRUE)
+        
+           print(PCs)
+           }
+           })
+       
+       # Biplot 
+       output$biplot <- renderPlot({
+           if (length(input$PCAVar) > 1){
+           PCs <- prcomp(select(data, !!!input$PCAVar), scale = TRUE)
+           biplot(PCs, xlabs = rep(".", nrow(data)), cex = 1.2)
+           } else {}
+       })
+       
+       # Appropriateness of PCs
+       output$approp <- renderPlot({
+           if(length(input$PCAVar) > 1){
+           PCs <- prcomp(select(data, !!!input$PCAVar), scale = TRUE)
+           # Plots to see appropriateness of PCs 
+           par(mfrow = c(1,2))
+           plot(PCs$sdev^2/sum(PCs$sdev^2), xlab = "Principal Component", 
+                ylab = "Proportion of Variance Explained", ylim = c(0,1), 
+                type = 'b')
+           plot(cumsum(PCs$sdev^2/sum(PCs$sdev^2)), xlab = "Principal Component", ylab = "Cum. Prop of Variance Explained", ylim = c(0,1), type = 'b')
+           } else {}
+       })
+      
+# -------------- TAB 4 ------------ #
+    output$linreg <- renderPrint({
+        # Data for Regression 
+        regData <- data %>% select(G3, sex, age, school, absences, studytime, failures)
+        
+        if(length(input$regX) > 0){
+            model <- lm(as.formula(paste("G3 ~ ", paste(input$regX, collapse = "+"))), data = regData)
+            print(model$coefficients)
+        } else {}
+    })
+
+# -------------- TAB 5 ------------ # 
+    # Output for Tab 5 
+       
+    output$tab5 <- renderGvis({
+        gvisTable(data)
+    })
+    
+    output$saveData <- downloadHandler(
+        filename<- function(){
+            paste("data", Sys.Date(), ".csv", sep = "")
+        },
+        content <- function(file){
+            write.csv(data, file)
+        }
+    )
+})
+    
---
+++ ui.R
@@ -0,0 +1,181 @@
+# Sarah McLaughlin 
+# ST558 Project 3
+# July 23, 2020 
+#
+
+library(shiny)
+library(shinydashboard)
+
+# UI 
+shinyUI(fluidPage(
+
+    # Application title
+   titlePanel("Using Data Mining to Predict Secondary School Performance in Math Class"),
+
+    # Create Dashboard  
+    dashboardPage(
+        dashboardHeader(title = "Dashboard"),
+        dashboardSidebar(
+            sidebarMenu(
+    # Create Tabs 
+                # First Tab 
+                menuItem("Introduction", tabName = "Intro"),
+                # Second Tab 
+                menuItem("Data Exploration", tabName = "DE", 
+                         menuSubItem("Categorial Data", tabName = "Cat"), 
+                         menuSubItem("Quantitative Data", tabName = "Quant")), 
+                # Third Tab 
+                menuItem("Principal Component Analysis", tabName = "PCA"), 
+                # Fourth Tab 
+                menuItem("Modeling", tabName = "Model", 
+                         menuSubItem("Linear Regression Model", tabName = "LinReg"), 
+                         menuSubItem("Classification Tree", tabName = "Class")), 
+                # Fifth Tab 
+                menuItem("Data", tabName = "D")
+            )
+        ), 
+        
+        # Dashboard Body 
+        dashboardBody(
+            tabItems(
+      # ---------------- First Tab ----------------- # 
+                
+      # ---------------- Second Tab ---------------- # 
+                # Categorical Data Analysis Tab 
+                tabItem(tabName = "Cat", 
+                        fluidPage(
+                            box(title = "Analysis of Categorical Data", 
+                                # Select Widget Input 
+                                selectInput("CatTable", 
+                                           "Pick Variable", 
+                                            choices = list("final", "letter", "sex",
+                                                           "age", "school", "Pstatus", "internet", "higher"), 
+                                            multiple = FALSE)), 
+                            # One Way Table 
+                            box(title = "One Way Table", 
+                                tableOutput("catTable")), 
+                            
+                            # Bar Graphs 
+                            box(title = "Bar Graphs", 
+                                # Click to Sort by a Variable 
+                                checkboxInput("color", "Color by Second Variable?"), 
+                                    
+                                    conditionalPanel(condition = "input.color", 
+                                        selectInput("CatCol", 
+                                            "Color by Variable", 
+                                            choices = list("sex", "school", "Pstatus", "internet", "higher"), 
+                                            multiple = FALSE)), 
+                                plotOutput("catGraph"))
+                        )),
+                
+                # Quantitative Data Analysis Tab 
+                tabItem(tabName = "Quant", 
+                        fluidPage( 
+                            # Title Box
+                            box(title = "Six Number Summary", 
+                            # Click to analyze only one variable  
+                                checkboxInput("one", "Analyze One Numeric Variable?"), 
+                                    conditionalPanel(condition = "input.one", 
+                                                     selectInput("oneNum", 
+                                                                 "Variable", 
+                                                                 choices = list("G3", "age", "absences", "Medu",
+                                                                                "Fedu", "famrel", "studytime",
+                                                                                "failures", "traveltime", "Walc",
+                                                                                "health"), multiple = FALSE))), 
+                                uiOutput("numsums"), 
+                            br(), 
+                            
+                            box(title = "Histogram of One Variable",
+                                plotOutput("Hist")),
+                            
+                            # Scatterplot Box 
+                            box(title = "Scatterplot for Two Variables", 
+                                selectInput("xvar", "X", 
+                                            choices = list("G3", "age", "absences", "Medu",
+                                                           "Fedu", "famrel", "studytime",
+                                                           "failures", "traveltime", "Walc",
+                                                           "health" ), multiple = FALSE), 
+                                selectInput("yvar", "Y", 
+                                            choices = list("G3", "age", "absences", "Medu",
+                                                           "Fedu", "famrel", "studytime",
+                                                           "failures", "traveltime", "Walc",
+                                                           "health"), multiple = FALSE), 
+                                plotOutput("scatter", click = "plot_click"),
+                                verbatimTextOutput("info"),
+                            
+                                # Save Scatter Plot 
+                                downloadButton("savescat", "Download")
+                            ))),
+                
+          # ---------------- Third Tab ---------------- #  
+                tabItem(tabName = "PCA", 
+                        fluidPage(
+                          # PCA Output 
+                            box(title = "Principal Component Analysis", 
+                                selectInput("PCAVar", "Variables", 
+                                            choices = list("G3", "age", "absences", "Medu",
+                                                           "Fedu", "famrel", "studytime",
+                                                           "failures", "traveltime", "Walc",
+                                                           "health"), multiple = TRUE)),
+                            box(title = "PCA Output", 
+                                verbatimTextOutput("PCAOutput")), 
+                          # Biplot 
+                            box(title = "BiPlot", 
+                                plotOutput("biplot")), 
+                          
+                          # Appropriateness of PCs
+                            box(title = "Appropriateness of PCs", 
+                                plotOutput("approp"))
+                        )
+                        ),
+          # ---------------- Fourth Tab ---------------- # 
+                # Linear Regression Tab
+                tabItem(tabName = "LinReg", 
+                        fluidPage(
+                          box(title = "Linear Regression", 
+                              selectInput("regX", "Pick X Variable(s) for Linear Regression", 
+                                          choices = list("sex", "age", "school", 
+                                                         "absences", "studytime", "failures"), 
+                                          multiple = TRUE), 
+                              verbatimTextOutput("linreg")), 
+                          box(title = "Values for Prediction", 
+                              h5("Insert values for prediction below"), 
+                              # Var 1 G3, sex, age, school, absences, studytime, failures
+                              h6("For sex, Female = 0, Male = 1"),
+                            numericInput("sexValue","Sex", value = 0, min = 0, max = 1, step = 1), 
+                            numericInput("ageValue", "Age", value = 0, min = 15, max = 22, step = 1), 
+                              h6("For school, Gabriel Pereira = 0, Mousinho da Silveira = 1"), 
+                            numericInput("schoolValue", "School", value = 0, min = 0, max = 1, step = 1 ), 
+                            numericInput("absencesValue", "Absences", value = 0, min = 0, max = 75, step = 1), 
+                              h6("For study time, Less Than Two Hours = 1, 2-5 Hours = 2, 5-10 Hours = 3, More than 10 Hours = 4"),
+                            numericInput("studytimeValue", "Studytime", value = 0, min = 0, max = 4, step = 1 ), 
+                            numericInput("failuresValue", "Number of Failures", value = 0, min = 0, max = 3, step = 1)
+                        ))),
+          
+          
+          # ---------------- Fifth Tab ---------------- # 
+                tabItem(tabName = "D",
+                        # Widget to Save Data Set
+                        fluidRow(box( 
+                          downloadButton("saveData", "Save Dataset")), 
+                          
+                        fluidRow(
+                          # Show DataSet
+                          box(title = "Data", 
+                              tableOutput("tab5")))
+
+                        
+                        
+            )
+        )
+    )
+)
+)
+) #FluidPage
+) #ShinyUI
+
+ 
+    
+
+
+
